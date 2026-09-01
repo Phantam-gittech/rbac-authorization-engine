ae97f6d219d622c0f6f90efcb6fbf878886b96bd
Atharva <vennuatharva@gmail.com>
2026-09-01 17:39:20 +0530

    Initial commit : Spring Boot project setup

---
+++ rbac-engine/rbac-engine/src/main/java/com/example/rbac_engine/RbacEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbac_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacEngineApplication.class, args);
+	}
+
+}
---
+++ rbac-engine/rbac-engine/src/test/java/com/example/rbac_engine/RbacEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbac_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
