d27319acce5706c38382228414a859292159d3d9
Atharva <vennuatharva@gmail.com>
2026-09-01 18:53:46 +0530

    Add User, Role, Permission, UserRole, RolePermission Entity

---
+++ rbac-engine/rbac-engine/src/main/java/com/example/rbac_engine/entity/operations/RolePermission.java
@@ -0,0 +1,29 @@
+package com.example.rbac_engine.entity.operations;
+
+import com.example.rbac_engine.entity.Permission;
+import com.example.rbac_engine.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role_permissions")
+public class RolePermission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "permission_id", nullable = false)
+    private Permission permission;
+}
---
+++ rbac-engine/rbac-engine/src/main/java/com/example/rbac_engine/entity/operations/UserRole.java
@@ -0,0 +1,31 @@
+package com.example.rbac_engine.entity.operations;
+
+import com.example.rbac_engine.entity.Role;
+import com.example.rbac_engine.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_role")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+
+}
